76b40a68ae4ef02940c4dfeb1c29d15e4f91a711
Nimrod Dayan <nimrod.dayan@gmail.com>
2020-08-22 10:46:52 +0300

    Moved code to plugin file

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -2,80 +2,12 @@ import UIKit
 import Flutter
 
 @UIApplicationMain
-class AppDelegate: FlutterAppDelegate, NetServiceBrowserDelegate, NetServiceDelegate {
-    private var netServiceBrowser: NetServiceBrowser!
-    private var services = [NetService]()
-    private var channel: FlutterMethodChannel!
-
-    override func application(
-            _ application: UIApplication,
-            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        print("listening for services...")
-        self.services.removeAll()
-        netServiceBrowser = NetServiceBrowser()
-        netServiceBrowser.delegate = self
-        let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
-        channel = FlutterMethodChannel(name: "com.nimroddayan/flutter_nsd", binaryMessenger: controller.binaryMessenger)
-        channel.setMethodCallHandler { call, result in
-            switch call.method {
-            case "startDiscovery":
-                let args = call.arguments as? [String: Any]
-                let serviceType = args?["serviceType"] as? String
-                if serviceType == nil {
-                    result(FlutterError(code: "1001", message: "Service type cannot be null", details: nil))
-                    return
-                }
-
-                self.startDiscovery(serviceType!)
-                result(nil)
-            case "stopDiscovery":
-                self.stopDiscovery()
-                result(nil)
-            default:
-                result(FlutterMethodNotImplemented)
-            }
-        }
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-    private func startDiscovery(_ serviceType: String) {
-        print("Starting discovery for \(serviceType)")
-        netServiceBrowser.searchForServices(ofType: serviceType, inDomain: "")
-    }
-
-    private func stopDiscovery() {
-        print("Stopping discovery")
-        netServiceBrowser.stop()
-    }
-
-    func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
-        print("adding a service")
-        self.services.append(service)
-        if !moreComing {
-            self.updateInterface()
-        }
-    }
-
-    private func updateInterface() {
-        for service in self.services {
-            if service.port == -1 {
-                print("service \(service.name) of type \(service.type)" +
-                        " not yet resolved")
-                service.delegate = self
-                service.resolve(withTimeout: 10)
-            } else {
-                print("service \(service.name) of type \(service.type)," +
-                        "port \(service.port), addresses \(service.addresses)")
-            }
-        }
-    }
-
-    func netServiceDidResolveAddress(_ sender: NetService) {
-        print("Found service: \(String(describing: sender.hostName)) \(sender.port) \(sender.name)")
-        channel.invokeMethod(
-                "onServiceResolved",
-                arguments: ["ip": sender.hostName, "port": sender.port, "name": sender.name])
-    }
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
--- ios/Classes/SwiftFlutterNsdPlugin.swift
+++ ios/Classes/SwiftFlutterNsdPlugin.swift
@@ -1,35 +1,82 @@
 import Flutter
 import UIKit
 
-public class SwiftFlutterNsdPlugin: NSObject, FlutterPlugin {
-  var channel: FlutterMethodChannel!
-  var netServiceBrowser: NetServiceBrowser!
-  var services = [NetService]()
+public class SwiftFlutterNsdPlugin: NSObject, FlutterPlugin, NetServiceBrowserDelegate, NetServiceDelegate {
+    private var netServiceBrowser: NetServiceBrowser!
+    private var services = [NetService]()
+    private var channel: FlutterMethodChannel
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    channel = FlutterMethodChannel(name: "com.nimroddayan/flutter_nsd", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterNsdPlugin()
-    self.services.removeAll()
-    netServiceBrowser = NetServiceBrowser()
-    netServiceBrowser.delegate = self
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "startDiscovery":
-      let serviceType = call.arguments["serviceType"] as? String?
-      if serviceType == nil {
-        result.error("1001", "service type cannot be null")
-        return
-      }
-
-      startDiscovery(serviceType)
-      result.success(nil)
+    init(channel: FlutterMethodChannel) {
+        self.channel = channel
+        self.services.removeAll()
+        netServiceBrowser = NetServiceBrowser()
+        super.init()
     }
-  }
 
-  private func startDiscovery(_ serviceType: String) {
-    netServiceBrowser.searchForServices(ofType: serviceType, inDomain: "")
-  }
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let flutterNsdChannel = FlutterMethodChannel(name: "com.nimroddayan/flutter_nsd", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterNsdPlugin(channel: flutterNsdChannel)
+        registrar.addMethodCallDelegate(instance, channel: flutterNsdChannel)
+    }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "startDiscovery":
+            let args = call.arguments as? [String: Any]
+            let serviceType = args?["serviceType"] as? String
+            if serviceType == nil {
+                result(FlutterError(code: "1001", message: "Service type cannot be null", details: nil))
+                return
+            }
+
+            self.startDiscovery(serviceType!)
+            result(nil)
+        case "stopDiscovery":
+            self.stopDiscovery()
+            result(nil)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    private func startDiscovery(_ serviceType: String) {
+        print("Starting discovery for \(serviceType)")
+        netServiceBrowser.delegate = self
+        netServiceBrowser.searchForServices(ofType: serviceType, inDomain: "")
+    }
+
+    private func stopDiscovery() {
+        print("Stopping discovery")
+        netServiceBrowser.delegate = nil
+        netServiceBrowser.stop()
+    }
+
+    private func updateInterface() {
+        for service in services {
+            if service.port == -1 {
+                print("service \(service.name) of type \(service.type)" +
+                        " not yet resolved")
+                service.delegate = self
+                service.resolve(withTimeout: 10)
+            } else {
+                print("service \(service.name) of type \(service.type)," +
+                        "port \(service.port), addresses \(service.addresses)")
+            }
+        }
+    }
+
+    public func netServiceBrowser(_ browser: NetServiceBrowser, didFind service: NetService, moreComing: Bool) {
+        print("adding a service")
+        services.append(service)
+        if !moreComing {
+            self.updateInterface()
+        }
+    }
+
+    public func netServiceDidResolveAddress(_ sender: NetService) {
+        print("Found service: \(String(describing: sender.hostName)) \(sender.port) \(sender.name)")
+        channel.invokeMethod(
+                "onServiceResolved",
+                arguments: ["ip": sender.hostName, "port": sender.port, "name": sender.name])
+    }
 }
