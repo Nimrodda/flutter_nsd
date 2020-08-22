d81d58c8cdb2d218e5e6ac5798d2e80b4541f851
Nimrod Dayan <nimrod.dayan@gmail.com>
2020-08-22 10:32:08 +0300

    Initial ios impl

--- ios/Classes/SwiftFlutterNsdPlugin.swift
+++ ios/Classes/SwiftFlutterNsdPlugin.swift
@@ -2,13 +2,34 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterNsdPlugin: NSObject, FlutterPlugin {
+  var channel: FlutterMethodChannel!
+  var netServiceBrowser: NetServiceBrowser!
+  var services = [NetService]()
+
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_nsd", binaryMessenger: registrar.messenger())
+    channel = FlutterMethodChannel(name: "com.nimroddayan/flutter_nsd", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterNsdPlugin()
+    self.services.removeAll()
+    netServiceBrowser = NetServiceBrowser()
+    netServiceBrowser.delegate = self
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "startDiscovery":
+      let serviceType = call.arguments["serviceType"] as? String?
+      if serviceType == nil {
+        result.error("1001", "service type cannot be null")
+        return
+      }
+
+      startDiscovery(serviceType)
+      result.success(nil)
+    }
+  }
+
+  private func startDiscovery(_ serviceType: String) {
+    netServiceBrowser.searchForServices(ofType: serviceType, inDomain: "")
   }
 }
