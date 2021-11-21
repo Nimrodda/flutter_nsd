c6b36e0e6e426e592533295154ec480e432e1c3f
Nimrod Dayan <nimrod.dayan@gmail.com>
2021-11-21 23:23:54 +0200

    Added MacOS desktop support

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_nsd
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  SwiftFlutterNsdPlugin.register(with: registry.registrar(forPlugin: "SwiftFlutterNsdPlugin"))
+}
