bd45863b6ecc916197bec478a9b080cac5c423b2
Nimrod Dayan <Nimrodda@users.noreply.github.com>
2025-04-28 22:41:43 +0300

    Fixed Android build

--- android/src/main/kotlin/com/nimroddayan/flutternsd/FlutterNsdPlugin.kt
+++ android/src/main/kotlin/com/nimroddayan/flutternsd/FlutterNsdPlugin.kt
@@ -30,7 +30,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import timber.log.Timber
 import java.util.LinkedList
 import java.util.Queue
@@ -61,24 +60,6 @@ class FlutterNsdPlugin : FlutterPlugin, MethodCallHandler {
     mainHandler = Handler(Looper.getMainLooper())
     channel.setMethodCallHandler(this)
     Timber.d("Plugin initialized successfully")
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @Suppress("unused")
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "com.nimroddayan/flutter_nsd")
-      channel.setMethodCallHandler(FlutterNsdPlugin())
-    }
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
